e2d3d91182709c5b558aacae4dc074495a20764d
prabal255 <prabalsurana00@gmail.com>
2021-06-06 20:22:06 +0530

    Added C# Case Study

---
+++ C#_Case_Study/Dao/CartEmptyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Cognizant.Truyum.Dao
+{
+    class CartEmptyException:Exception
+    {
+        public CartEmptyException()
+        {
+            Console.WriteLine("Empty cart");
+        }
+    }
+}
---
+++ C#_Case_Study/Dao/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Com.Cognizant.Truyum.Dao
+{
+    public class Helper
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString;
+            }
+        }
+        public static SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            return con;
+
+        }
+
+    }
+}
